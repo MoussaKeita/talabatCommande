5962e5f3ccf052889e2ed625c96b279dd9799b71
Keita Moussa <moussakeitamoussakeita@gmail.com>
2019-03-31 12:16:43 +0100

    verifie si c'est ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommandesComponent } from './commandes/commandes.component';
+import { CommandeCreateComponent } from './commandes/commande-create/commande-create.component';
+import { CommandeListComponent } from './commandes/commande-list/commande-list.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommandesComponent,
+    CommandeCreateComponent,
+    CommandeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
